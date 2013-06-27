da76e786ab99031a9da1c37028eb045b4ab9844b
Kevin Sullivan <sideslapd@gmail.com>
2013-06-26 19:18:45 -0700

    Messed around with the API wrapper a bit

--- src/instahash/InstaHash.java
+++ src/instahash/InstaHash.java
@@ -1,9 +1,45 @@
 package instahash;
 
+import org.jinstagram.*;
+import org.jinstagram.entity.tags.TagSearchFeed;
+import org.jinstagram.entity.users.basicinfo.*;
+import org.jinstagram.exceptions.InstagramException;
+
+import com.sola.instagram.auth.InstagramAuthentication;
+
 public class InstaHash
 {
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      
+      // InstagramAuthentication auth =  new InstagramAuthentication(); 
+     
+      Instagram testInstance = new Instagram("a3d07a3cc65445e08ebd6dc958fe6b05");
+      
+      long userId = 3;
+      UserInfo userInfo;
+      try
+      {
+         TagSearchFeed testTagSearch = testInstance.searchTags("cat");
+         System.out.println(testTagSearch.toString());
+         testInstance.
+         
+         
+         userInfo = testInstance.getUserInfo(userId);
+         
+         UserInfoData userData = userInfo.getData();
+         System.out.println("id : " + userData.getId());
+         System.out.println("first_name : " + userData.getFirst_name());
+         System.out.println("last_name : " + userData.getLast_name());
+         System.out.println("profile_picture : " + userData.getProfile_picture());
+         System.out.println("website : " + userData.getWebsite());
+      } catch (InstagramException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+
    }
 }
